aab47eb2552c74ce2b4840972270c556e87a6a37
ryrd <rdridhani@gmail.com>
2022-08-26 09:25:02 +0800

    add few animation

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState, useRef } from 'react'
-import {motion, useScroll, useTransform} from 'framer-motion'
+import {motion, useScroll, useTransform, Variants} from 'framer-motion'
 import Toggle from '../parts/Toggle'
 import arrow from '../../assets/arrow.svg'
 
@@ -25,6 +25,35 @@ import mountainOneNightWebp from '../../assets/header/night/mountain-1-night.web
 
 import mountainTwoNightPng from '../../assets/header/night/mountain-2-night.png'
 import mountainTwoNightWebp from '../../assets/header/night/mountain-2-night.webp'
+
+// -------------animation variant---------------
+const container: Variants = {
+  animate: {
+    transition: {
+      duration: 1,
+      delayChildren: .5,
+      staggerChildren: .25
+    }
+  },
+}
+
+const title = {
+  initial: {
+    opacity : 0,
+  },
+  animate: {
+    opacity : 1,
+  }
+}
+
+const scroll: Variants = {
+  initial: {
+    opacity: 0
+  },
+  animate: {
+    opacity: 1,
+  },
+}
 
 const Header = () => {
   // ---------video playback variable----------
@@ -108,54 +137,55 @@ const Header = () => {
     <div className='h-[90vh] md:portrait:h-[75vh] md:h-screen relative overflow-hidden'>
       <Toggle/>
 
-        <div className='h-full bg-cover'>
-          <video src={bgDay} id='vid' className='h-full md:w-screen object-cover'></video>
+      <div className='h-full bg-cover'>
+        <video src={bgDay} id='vid' className='h-full md:w-screen object-cover'></video>
+      </div>
+
+      <motion.picture className='absolute -bottom-[2vh] -right-[15vw] w-[150%] 
+                                          md:-bottom-[10vh] md:-right-[20vw] md:w-full
+                                  transition duration-300 ease-out md:transition-none'
+                      style={{y: mountainTwoVerticalValue, x: mountainTwoHorizontalValue}}>
+          <source src={mountainTwoWebp} type="image/webp"/>
+          <source src={mountainTwoPng} type="image/png"/>
+          <img src={mountainTwoWebp} />
+      </motion.picture>
+      
+      <motion.picture className='absolute -left-[30vw] -bottom-[10vw] w-[150%] 
+                                          md:-left-[15vw] md:-bottom-[13vh] md:w-full
+                                  transition duration-300 ease-out md:transition-none'
+                      style={{y: mountainOneVerticalValue, x: mountainOneHorizontalValue}}>
+          <source src={mountainOneWebp} type="image/webp"/>
+          <source src={mountainOnePng} type="image/png"/>
+          <img src={mountainOneWebp} />
+      </motion.picture>
+
+      <motion.div className='absolute top-[15vh] md:top-[13vh] text-white font-anton uppercase text-center left-1/2 -translate-x-1/2 drop-shadow-md transition duration-700 ease-out'
+                  ref={title} variants={container} initial='initial' animate='animate'>
+        <div className='flex items-center justify-center'>
+          <div className='h-[2px] portrait:w-[20vw] landscape:w-[10vw] bg-gradient-to-r from-transparent to-[#d9d9d9]'></div>
+          <h1 className='text-5xl md:text-7xl mx-4'>earth</h1>
+          <div className='h-[2px] portrait:w-[20vw] landscape:w-[10vw] bg-gradient-to-l from-transparent to-[#d9d9d9]'></div>
         </div>
+        <h1 className='text-7xl md:text-9xl'>wonderer</h1>
+      </motion.div>
 
-        <motion.picture className='absolute -bottom-[2vh] -right-[15vw] w-[150%] 
-                                            md:-bottom-[10vh] md:-right-[20vw] md:w-full
-                                   transition duration-300 ease-out md:transition-none'
-                        style={{y: mountainTwoVerticalValue, x: mountainTwoHorizontalValue}}>
-            <source src={mountainTwoWebp} type="image/webp"/>
-            <source src={mountainTwoPng} type="image/png"/>
-            <img src={mountainTwoWebp} />
-        </motion.picture>
-        
-        <motion.picture className='absolute -left-[30vw] -bottom-[10vw] w-[150%] 
-                                            md:-left-[15vw] md:-bottom-[13vh] md:w-full
-                                   transition duration-300 ease-out md:transition-none'
-                        style={{y: mountainOneVerticalValue, x: mountainOneHorizontalValue}}>
-            <source src={mountainOneWebp} type="image/webp"/>
-            <source src={mountainOnePng} type="image/png"/>
-            <img src={mountainOneWebp} />
-        </motion.picture>
+      <motion.picture className='absolute -left-[95vw] -bottom-[25vw] w-[280%]
+                                          md:portrait:-left-[6vw] md:portrait:-bottom-[0vh] md:portrait:w-[110%]
+                                          md:-left-[1vw] md:-bottom-[15vh] md:w-full
+                                  transition duration-300 ease-out md:transition-none'
+                      style={{y: personVerticalValue, x: personHorizontalValue}}>
+          <source srcSet={personDayWebp} type="image/webp"/>
+          <source srcSet={personDayPng} type="image/png"/>
+          <img src={personDayWebp} />
+      </motion.picture>
 
-        <div className='absolute top-[15vh] md:top-[13vh] text-white font-anton uppercase text-center left-1/2 -translate-x-1/2 drop-shadow-md transition duration-700 ease-out'
-             ref={title}>
-          <div className='flex items-center justify-center'>
-            <div className='h-[2px] portrait:w-[20vw] landscape:w-[10vw] bg-gradient-to-r from-transparent to-[#d9d9d9]'></div>
-            <h1 className='text-5xl md:text-7xl mx-4'>earth</h1>
-            <div className='h-[2px] portrait:w-[20vw] landscape:w-[10vw] bg-gradient-to-l from-transparent to-[#d9d9d9]'></div>
-          </div>
-          <h1 className='text-7xl md:text-9xl'>wonderer</h1>
-        </div>
+      <div className='absolute bottom-0 h-[80px] md:h-[100px] w-full header-gradient'></div>
 
-        <motion.picture className='absolute -left-[95vw] -bottom-[25vw] w-[280%]
-                                            md:portrait:-left-[6vw] md:portrait:-bottom-[0vh] md:portrait:w-[110%]
-                                            md:-left-[1vw] md:-bottom-[15vh] md:w-full
-                                   transition duration-300 ease-out md:transition-none'
-                        style={{y: personVerticalValue, x: personHorizontalValue}}>
-            <source srcSet={personDayWebp} type="image/webp"/>
-            <source srcSet={personDayPng} type="image/png"/>
-            <img src={personDayWebp} />
-        </motion.picture>
-
-        <div className='absolute bottom-0 h-[80px] md:h-[100px] w-full header-gradient'></div>
-
-        <div className='absolute bottom-[3vh] md:bottom-[5vh] left-1/2 -translate-x-1/2 font-oswald text-white font-light flex flex-col items-center text-sm md:text-base'>
-          <span>scroll down to explore</span>
-          <img src={arrow} alt="scroll down" className='w-6 scroll-down' />
-        </div>
+      <motion.div className='absolute bottom-[3vh] md:bottom-[5vh] left-1/2 -translate-x-1/2 font-oswald text-white font-light flex flex-col items-center text-sm md:text-base'
+                  variants={container} initial='initial' animate='animate'>
+        <motion.span variants={scroll}>scroll down to explore</motion.span>
+        <motion.img variants={scroll} src={arrow} alt="scroll down" className='w-6 scroll-down' />
+      </motion.div>
     </div>
   )
 }
