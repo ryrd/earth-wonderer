6db3c947020d902fc92bf4927b32146356ba928d
ryrd <rdridhani@gmail.com>
2022-09-02 18:54:02 +0800

    home heading change n about site first letter

--- src/components/parts/HomeHeading.tsx
+++ src/components/parts/HomeHeading.tsx
@@ -1,9 +1,8 @@
 const HomeHeading = ({text}: {text:string}) => {
   return (
-  <div className='flex items-center justify-center font-kaushan uppercase text-center z-10'>
-    <div className='h-[2px] portrait:w-[20vw] landscape:w-[15vw] bg-gradient-to-r from-transparent to-dark dark:to-white'/>
-    <h1 className='text-[5vw] md:text-[3.5vw] mx-2 md:mx-4 text-dark dark:text-white'>{text}</h1>
-    <div className='h-[2px] portrait:w-[20vw] landscape:w-[15vw] bg-gradient-to-l from-transparent to-dark dark:to-white'/>
+  <div className='relative flex items-center justify-center font-kaushan uppercase text-center z-10'>
+    <img src="/paint.webp" className="absolute top-0 left-1/2 -translate-x-1/2 portrait:-translate-y-[15%] landscape:-translate-y-[10%] w-[50vw] portrait:h-[6vh] md:w-[35vw] landscape:h-[14vh] z-0 dark:invert"/>
+    <h1 className='text-[5vw] md:text-[3.5vw] mx-2 md:mx-4 text-white dark:text-dark z-10'>{text}</h1>
   </div>
   )
 }
--- src/components/sections/AboutSite.tsx
+++ src/components/sections/AboutSite.tsx
@@ -18,7 +18,7 @@ const About = () => {
   return (
     <div className='relative h-auto flex justify-center md:justify-start items-center flex-col portrait:pb-[8vh] landscape:pb-[14vh] portrait:pt-8 landscape:pt-[6%]'
          ref={aboutImgsRef}>
-      <HomeHeading text='about'/>
+      <HomeHeading text='&nbsp;&nbsp;about&nbsp;&nbsp;&nbsp;'/>
       <p className='w-[70vw] md:w-1/2 font-oswald font-light md:text-[2.3vw] text-center mt-8 md:mt-10 z-10 dark:text-white dark:font-extralight'>
         <span className='font-oswand uppercase font-bold text-sm md:text-[2vw]'>Earth Wonderer</span> is a dummy website created by 
         <a href='https://ryrd.github.io' target='_blank'><span className='text-blue-900 font-gilda underline ml-1 md:ml-2'>ryrd</span>.</a> 
@@ -28,10 +28,10 @@ const About = () => {
       </p>
 
       <div className={`absolute top-0 h-[120px] w-full bg-gradient-to-b from-offwhite dark:from-dark to-transparent`}></div>
-
+      
       <motion.img src={about2} className='absolute -right-[8%] md:right-[12%] top-[45%] md:top-[35%] w-28 md:w-[16vw] z-0' 
                   style={{y: imgParallaxValue, rotate: imgRotateValue}}/>
-      <motion.img src={about1} className='absolute -left-[2%] md:left-[15%] bottom-[0%] md:bottom-[2%] w-24 md:w-[14vw] z-0' 
+      <motion.img src={about1} className='absolute -left-[5%] md:left-[13%] bottom-[0%] md:bottom-[2%] w-24 md:w-[14vw] z-0' 
                   style={{y: imgParallaxTwoValue, rotate: imgRotateTwoValue}}/>
     </div>
   )
